0c3ab48be935b1853b8caf7221b4c0a77f88faa7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-03-11 08:49:15 -0500

    removed href and added loadLink

--- Scripts/router.ts
+++ Scripts/router.ts
@@ -4,6 +4,7 @@ namespace core
     { 
         // Instance Variables
         private m_activeLink: string;
+        private m_linkData: string;
         private m_routingTable: string[];
 
         // constructors
@@ -21,6 +22,16 @@ namespace core
         set ActiveLink(link:string) 
         {
             this.m_activeLink = link;
+        }
+
+        get LinkData():string
+        {
+            return this.m_linkData;
+        }
+
+        set LinkData(data:string)
+        {
+            this.m_linkData = data;
         }
 
         // Public methods
